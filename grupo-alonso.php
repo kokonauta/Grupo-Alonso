c5049746ab50fa5400bd9ec6c7936282020c0be4
Kokonauta <lluesma@gmail.com>
2019-04-18 11:08:20 +0200

    First Commit

---
+++ grupo-alonso.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Grupo Alonso
+ * Plugin URI:      https://github.com/kokonauta/Grupo-Alonso.git
+ * Description:     Plugin para Grupo Alonso
+ * Author:          Jorge Lluesma Gil
+ * Author URI:      https://github.com/kokonauta
+ * Text Domain:     grupo-alonso
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Grupo_Alonso
+ */
+
+// Your code starts here.
